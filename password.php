48af800e7de9a90d37ff7c6a3d53e87ebdcf9de0
Rajanuar Oky <rajanuaroky@gmail.com>
2023-12-06 08:36:12 +0700

    secure pdf FIX

--- password.php
+++ password.php
@@ -5,14 +5,32 @@ if (isset($_POST['securePDF'])) {
     $pdfFile = isset($_FILES['pdfFile']['tmp_name']) ? $_FILES['pdfFile']['tmp_name'] : null;
     $password = $_POST['password'];
 
-    $outputFile = $uploadsDirectory . 'secured_pdf.pdf';
+    // Validasi input file
+    if (!file_exists($pdfFile) || !is_uploaded_file($pdfFile)) {
+        die('Invalid PDF file.');
+    }
+
+    $outputFile = $uploadsDirectory . pathinfo($_FILES['pdfFile']['name'], PATHINFO_FILENAME) . '_secured.pdf';
 
     // Code pdftk untuk mengamankan PDF dengan password
     $cmd = "pdftk \"$pdfFile\" output \"$outputFile\" owner_pw $password";
     exec($cmd);
 
-    // Tampilkan pesan sukses dan tautan download
-    echo "<h2>PDF Successfully Secured with Password</h2>";
-    echo "<p>Download your secured PDF: <a href='$outputFile' download>Download PDF</a></p>";
+    // Set header untuk auto download
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="' . basename($outputFile) . '"');
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: 0');
+
+    readfile($outputFile);
+
+    // Hapus file setelah di-download
+    unlink($pdfFile);
+    unlink($outputFile);
+
+    exit();
+}else {
+    die('Failed to process PDF file.');
 }
 ?>
