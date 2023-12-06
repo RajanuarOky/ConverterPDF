9fa5386baf9298fc7df407c2ae435062e36dd385
Rajanuar Oky <rajanuaroky@gmail.com>
2023-12-06 08:51:42 +0700

    watermark FIX

--- watermark.php
+++ watermark.php
@@ -5,14 +5,33 @@ if (isset($_POST['addWatermark'])) {
     $pdfFile = $_FILES['pdfFile']['tmp_name'];
     $watermarkFile = $_FILES['watermarkFile']['tmp_name'];
 
-    $outputFile = $uploadsDirectory . 'output_with_watermark.pdf';
+    // Validasi input file
+    if (!file_exists($pdfFile) || !is_uploaded_file($pdfFile) || !file_exists($watermarkFile) || !is_uploaded_file($watermarkFile)) {
+        die('Invalid PDF or watermark file.');
+    }
+
+    $outputFile = $uploadsDirectory . pathinfo($_FILES['pdfFile']['name'], PATHINFO_FILENAME) . '_watermark.pdf';
 
     // Code pdftk untuk menambahkan watermark
     $cmd = "pdftk \"$pdfFile\" background \"$watermarkFile\" output \"$outputFile\"";
     exec($cmd);
 
-    // Tampilkan pesan sukses dan tautan download
-    echo "<h2>PDF Successfully Watermarked</h2>";
-    echo "<p>Download your watermarked PDF: <a href='$outputFile' download>Download PDF</a></p>";
+    // Set header untuk auto download
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="' . basename($outputFile) . '"');
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: 0');
+
+    readfile($outputFile);
+
+    // Hapus file setelah di-download
+    unlink($pdfFile);
+    unlink($watermarkFile);
+    unlink($outputFile);
+
+    exit();
+} else {
+    die('Failed to process PDF file.');
 }
 ?>
