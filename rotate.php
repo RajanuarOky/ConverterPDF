6766fb4546f048c838028ee5571e816683311ea3
Rajanuar Oky <rajanuaroky@gmail.com>
2023-12-06 09:40:29 +0700

    coba benerin rotate

--- rotate.php
+++ rotate.php
@@ -1,22 +1,39 @@
 <?php
+if (isset($_POST['rotatePDF'])) {
+    $uploadsDirectory = './';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $uploadDir = "./";
-    $uploadFile = $uploadDir . basename($_FILES["pdfFile"]["name"]);
+    $pdfFile = $_FILES['pdfFile']['tmp_name'];
+    $rotationAngle = $_POST['rotationAngle'];
 
-    if (move_uploaded_file($_FILES["pdfFile"]["tmp_name"], $uploadFile)) {
-        $rotationAngle = $_POST["rotationAngle"];
-        $outputFile = $uploadDir . "rotated_" . time() . ".pdf";
-
-        $command = "pdftk $uploadFile cat 1-end$rotationAngle output $outputFile";
-        shell_exec($command);
-
-        // Provide a link to download the rotated PDF
-        echo "<h2>PDF Rotated Successfully!</h2>";
-        echo "<p>Download your rotated PDF: <a href='$outputFile' download>Download</a></p>";
-    } else {
-        echo "<h2>Failed to upload file.</h2>";
+    // Validasi input file
+    if (!file_exists($pdfFile) || !is_uploaded_file($pdfFile)) {
+        die('Invalid PDF file.');
     }
-}
 
+    // Validasi sudut rotasi
+    if (!in_array($rotationAngle, ['90', '180', '270'])) {
+        die('Invalid rotation angle.');
+    }
+
+    $outputFile = $uploadsDirectory . pathinfo($_FILES['pdfFile']['name'], PATHINFO_FILENAME) . '_rotated.pdf';
+
+    // Code pdftk untuk merotasi PDF
+    $cmd = "pdftk \"$pdfFile\" cat 1-end$rotationAngle output \"$outputFile\"";
+    exec($cmd);
+
+    // Set header untuk auto download
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="' . basename($outputFile) . '"');
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: 0');
+
+    readfile($outputFile);
+
+    // Hapus file setelah di-download
+    unlink($pdfFile);
+    unlink($outputFile);
+
+    exit();
+}
 ?>
